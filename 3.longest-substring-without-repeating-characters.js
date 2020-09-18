c510b638d1599e2796ce0bb6f768e7dccdbe6917
fstar-mac <hhbuhuiai@163.com>
2020-09-18 22:28:30 +0800

    add 3[medium]

--- 3.longest-substring-without-repeating-characters.js
+++ 3.longest-substring-without-repeating-characters.js
@@ -10,7 +10,22 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  // TODO: to finish
+  if (s.length <= 1) return s.length
+
+  let max = 0
+  const hash = {}
+  let left = 0
+  for (let i = 0; i < s.length; i++) {
+    const c = s[i]
+    // vaild index range is [left, i]
+    if (hash[c] !== undefined && hash[c] >= left) {
+      max = Math.max(max, i - left)
+      left = hash[c] + 1
+    }
+    hash[c] = i
+  }
+  max = Math.max(max, s.length - left)
+  return max
 };
 // @lc code=end
 
