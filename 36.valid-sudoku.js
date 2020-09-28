4282a5349b8e202d7dfb4143be8e1110edb1d978
fstar-mac <hhbuhuiai@163.com>
2020-09-28 23:05:45 +0800

    update 36

--- 36.valid-sudoku.js
+++ 36.valid-sudoku.js
@@ -10,7 +10,47 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    
+  // row
+  let row
+  let hash = {}
+  for (let i = 0; i < 9; i++) {
+    hash = {}
+    row = board[i]
+    for (let j = 0; j < 9; j++) {
+      const num = row[j]
+      if (num === '.') continue
+      if (hash[num]) return false
+      hash[num] = true
+    }
+  }
+  // column
+  for (let i = 0; i < 9; i++) {
+    hash = {}
+    for (let j = 0; j < 9; j++) {
+      const num = board[j][i]
+      if (num === '.') continue
+      if (hash[num]) return false
+      hash[num] = true
+    }
+  }
+  // 3x3
+  for (let i = 0; i < 9; i+=3) {
+    for (let j = 0; j < 9; j+=3) {
+      // (i, j) is left top of small rect
+      hash = {}
+      for (let m = 0; m < 3; m++) {
+        for (let n = 0; n < 3; n++) {
+          // (i + m, j + n)
+          const num = board[i + m][j + n]
+          if (num === '.') continue
+          if (hash[num]) return false
+          hash[num] = true
+        }
+      }
+    }
+  }
+  return true 
 };
+
 // @lc code=end
 
