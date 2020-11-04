bd84d5130cf441224b5729e2258d2ed213120b3e
fstar-mac <hhbuhuiai@163.com>
2020-11-04 22:36:31 +0800

    add 98（代码思路不是最优，代码不够优雅）

--- 98.validate-binary-search-tree.go
+++ 98.validate-binary-search-tree.go
@@ -13,8 +13,54 @@
  *     Right *TreeNode
  * }
  */
-func isValidBST(root *TreeNode) bool {
-    
-}
-// @lc code=end
+// package main
 
+// type TreeNode struct {
+// 	Val   int
+// 	Left  *TreeNode
+// 	Right *TreeNode
+// }
+
+func isValidBST(root *TreeNode) bool {
+	isBST := true
+	if root == nil {
+		return true
+	}
+	f(root, &isBST)
+	return isBST
+}
+
+func f(node *TreeNode, isBST *bool) (int, int) {
+	if *isBST == false {
+		return 0, 0
+	}
+
+	var min, max int
+	if node.Left == nil {
+		min = node.Val
+	} else {
+		leftMin, leftMax := f(node.Left, isBST)
+		if node.Val > leftMax {
+			min = leftMin
+		} else {
+			*isBST = false
+			return 0, 0
+		}
+	}
+
+	if node.Right == nil {
+		max = node.Val
+	} else {
+		rightMin, rightMax := f(node.Right, isBST)
+		if node.Val < rightMin {
+			max = rightMax
+		} else {
+			*isBST = false
+			return 0, 0
+		}
+	}
+
+	return min, max
+}
+
+// @lc code=end
